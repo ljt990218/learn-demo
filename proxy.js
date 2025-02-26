ed9dfef8bf460960d36a3e61c2e00bf85b1d0ce3
ljt990218 <2511590093@qq.com>
2025-02-26 18:38:24 +0800

    first commit

---
+++ proxy.js
@@ -0,0 +1,32 @@
+let obj = {
+  a: 1,
+  b: 2,
+  c: {
+    d: 4,
+    e: 5,
+  },
+  f: 6,
+}
+
+const proxy = new Proxy(obj, {
+  get(target, k) {
+    console.log('get', k)
+    return target[k]
+  },
+
+  set(target, k, v) {
+    if (target[k] === v) return
+
+    console.log('set', k)
+    target[k] = v
+  },
+
+  deleteProperty(target, k) {
+    console.log('delete', k)
+    delete target[k]
+  },
+})
+
+proxy.a = 3
+delete proxy.f
+console.log(proxy)
